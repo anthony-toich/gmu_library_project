73d959b911f7a1080b6a1073c6b195ed3027e85c
anthony-toich <atoich@masonlive.gmu.edu>
2015-12-10 19:16:06 -0800

    Modified schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111030027) do
+ActiveRecord::Schema.define(version: 20151211025048) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
