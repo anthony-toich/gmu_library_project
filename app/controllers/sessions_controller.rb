e1e96530a0eb6c19801947d34a7a31c608ef1c37
anthony-toich <atoich@masonlive.gmu.edu>
2015-12-18 12:04:50 -0800

    Modified flash messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
-      redirect_to '/login'
+      redirect_to '/login', notice: "Invalid user name/password."
     end
   end
 
   def destroy
     session[:user_id]=nil
-    redirect_to '/login'
+    redirect_to '/login', notice: "Successfully logged out."
   end
 end
