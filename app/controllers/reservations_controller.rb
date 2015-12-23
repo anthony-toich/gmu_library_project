3f67e6a387e5289573606f1a6401c93004d4b829
anthony-toich <atoich@masonlive.gmu.edu>
2015-12-22 18:41:39 -0800

    Modified reservations controller.  Start on reservations.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,7 +4,12 @@ before_filter :authorize
 
 def index
 @available_at = Time.now
+  if current_user.admin == true
      @reservations = Reservation.order(:due_on)
+   else
+     #Show my reservations
+      @reservations = Reservation.where(["user_id = ?", current_user.id])
+   end
   end
 
 before_action :set_reservation, only: [:show, :edit, :update, :destroy ]
@@ -25,21 +30,30 @@ def create
 
 
   if @reservation.save
-    redirect_to @reservation , notice: "Reservation was created!"
+    #Decrement the number of books in the library.
+
+    redirect_to reservations_url , notice: "Reservation was created!"
   else
-  redirect_to @reservation, notice: "Reservation was NOT created!"
+    redirect_to reservations_url, notice: "Reservation was NOT created!"
   end
+end
+
+def destroy
+
+    @reservation = Reservation.find(params[:book_id])
+    @reservation.destroy
+    redirect_to reservations_url
 end
 
 
 
 private
-def reservation_params
- params.require(:reservation).permit(:user_id, :book_id, :reserved_on, :due_on)
-end
+  def reservation_params
+   params.require(:reservation).permit(:user_id, :book_id, :reserved_on, :due_on)
+  end
 
-def set_reservation
-@reservation = Reservation.find(params[:id])
-end
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
 
 end
